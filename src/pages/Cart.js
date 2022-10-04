308f1d63ddb141ab0f5e42cf68f0285f3e32dc71
Juan Pablo Soro <sorojuanpablo@gmail.com>
2022-10-04 09:08:46 -0300

    Post of orders in Firebase DB

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,9 +2,26 @@ import React from "react";
 import { useCartContext } from "../components/CartProvider"
 import { CartWithoutProducts } from "../components/CartWithoutProducts";
 import ItemCart from "../components/ItemCart";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const Cart = () => {
     const { cart, clearCart, totalPrice } = useCartContext();
+
+    const order = {
+        buyer: {
+            name: "Juan Pablo",
+            email: "sorojuanpablo@gmail.com",
+            address: "Avenida Cabildo 1398"
+        },
+        items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
+        total: totalPrice()
+    }
+
+    const postClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, "orders");
+        addDoc(ordersCollection, order).then(({ id }) => console.log(`El ID generado es: ${id}`));
+    }
 
     if (cart.length === 0) {
         return (
@@ -20,6 +37,7 @@ const Cart = () => {
 
             <p>Precio total: {totalPrice()} </p>
             <button onClick={() => clearCart()}>Eliminar todos los productos</button>
+            <button onClick={postClick}>Generar orden</button>
         </>
     )
 
